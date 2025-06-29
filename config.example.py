afca08f2136273fede893168075b17654db8dbfe
ada2804 <anujasthana2804@gmail.com>
2025-06-29 19:13:07 +0530

    Example Configuration file added

---
+++ config.example.py
@@ -0,0 +1,12 @@
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'your_mysql_username',
+    'password': 'your_mysql_password',
+    'database': 'employee',
+    'auth_plugin': 'mysql_native_password'
+}
+
+APP_CREDENTIALS = {
+    'username': 'admin',
+    'password': 'password'
+}
